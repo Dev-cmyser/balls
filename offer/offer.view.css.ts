6495b102d8a894cb11982a1e23285697bb954d6f
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-02-19 22:33:50 +0300

    ыав
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

---
+++ offer/offer.view.css.ts
@@ -0,0 +1,16 @@
+namespace $.$$ {
+	
+	$mol_style_define( $giper_balls_offer, {
+		
+		Content: {
+			padding: $mol_gap.text,
+			maxWidth: '800px',
+			margin: {
+				left: 'auto',
+				right: 'auto',
+			},
+		},
+		
+	} )
+	
+}
