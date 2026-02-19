65142885c957346307048f8f8c703e515bdcb600
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-02-19 22:17:21 +0300

    выа
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

---
+++ requisites/requisites.view.css.ts
@@ -0,0 +1,23 @@
+namespace $.$$ {
+	
+	$mol_style_define( $giper_balls_requisites, {
+		
+		Info: {
+			padding: $mol_gap.text,
+		},
+		
+		OGRNIP: {
+			margin: {
+				top: $mol_gap.text,
+			},
+		},
+		
+		INN: {
+			margin: {
+				top: $mol_gap.text,
+			},
+		},
+		
+	} )
+	
+}
